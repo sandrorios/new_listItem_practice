7275ade553b4dd833d7c25df750eb2186ae40757
sandrorios <sandroderios@hotmail.com>
2023-12-03 08:57:29 -0500

    added variable declarations to store various elements from the HTML document

---
+++ script.js
@@ -0,0 +1,5 @@
+let formItem = document.getElementById('form-item');
+let listItem = document.getElementById('list-item');
+let inputField = document.getElementById('input-field');
+let clearBtn = document.getElementById('clear');
+let filter = document.getElementById('filter');
