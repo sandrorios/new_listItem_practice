7299c0ac491121d360c876ee78fbd57e4067a3ef
sandrorios <sandroderios@hotmail.com>
2023-12-03 09:40:56 -0500

    added button elements and change things around for removing localStorage items

--- script.js
+++ script.js
@@ -21,11 +21,33 @@ function addItemToDOM(item){
     let li = document.createElement('li');
     li.appendChild(document.createTextNode(item));
     listItem.appendChild(li);
-
+    let button = createButton('remove-item text-red');
+    li.appendChild(button);
     inputField.value = '';
     inputField.focus();
 }
 
+function createButton(classes){
+    let button = document.createElement('button');
+    button.className = classes;
+    let icon = createIcon('fa-solid fa-xmark');
+    button.appendChild(icon)
+    return button;
+}
+
+function createIcon(classes){
+    let icon = document.createElement('i');
+    icon.className = classes;
+    return icon;
+}
+
+function removeItem(item) {
+    item.remove();
+    removeItemFromStorage(item.textContent);
+    // if(e.target.parentElement.classList.contains('remove-item')){
+    //     e.target.parentElement.parentElement.remove();
+    // }
+}
 function addItemToStorage(item){
     const itemsFromStorage = getItemsFromStorage()
     itemsFromStorage.push(item);
@@ -42,6 +64,19 @@ function getItemsFromStorage() {
         return itemsFromStorage;
 }
 
+function removeItemFromStorage(item){
+    let itemsFromStorage = getItemsFromStorage();
+    itemsFromStorage = itemsFromStorage.filter((i) => i !== item);
+
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
+function onClickItem(e) {
+    if(e.target.parentElement.classList.contains('remove-item')){
+        removeItem(e.target.parentElement.parentElement);
+    }
+}
 
 
 formItem.addEventListener('submit', onAddItemSubmit);
+listItem.addEventListener('click', onClickItem);
